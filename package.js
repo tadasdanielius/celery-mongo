4e82513bec3e40284f963f53b8255f9c98234034
Tadas <tadas.danielius@gmail.com>
2015-08-19 15:37:40 +0100

    added github url

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Interface to Celery - distributed task queue',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/tadasdanielius/celery-mongo',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
